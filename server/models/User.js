6b60883d2aba0e57d7b6726c97d3a059c82d8c9b
hassanShakur <hass.shakurhassan@gmail.com>
2023-10-29 17:59:02 +0300

    add user schema

--- server/models/User.js
+++ server/models/User.js
@@ -1,1 +1,22 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    minlength: 4,
+    required: true,
+  },
+  role: {
+    type: String,
+    default: 'User',
+    required: true,
+  },
+});
+
+const User = mongoose.model('user', userSchema);
+module.exports = User;
