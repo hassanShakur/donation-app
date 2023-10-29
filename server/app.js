760270ba245389d4a96d89cfacaea0028b68d6d3
hassanShakur <hass.shakurhassan@gmail.com>
2023-10-29 21:24:48 +0300

    add template pug files

--- server/app.js
+++ server/app.js
@@ -1,16 +1,43 @@
 const express = require('express');
 const setupApp = require('./appSetup');
-const cookieParser = require('cookie-parser')
+const cookieParser = require('cookie-parser');
 const authRouter = require('./routers/auth');
 
 const app = express();
 
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
+app.set('view engine', 'pug');
 
 setupApp(app);
 
 app.use('/api/auth', authRouter);
+
+app.get('/', (_, res) => {
+  res.render('index');
+});
+
+app.get('/login', (_, res) => {
+  res.render('login');
+});
+
+app.get('/register', (_, res) => {
+  res.render('register');
+});
+
+app.get('/dashboard', (_, res) => {
+  res.render('dashboard');
+});
+
+app.get('/profile', (_, res) => {
+  res.render('profile');
+});
+
+app.get('/logout', (_, res) => {
+  res.cookie('jwt', '', { maxAge: 1 });
+  res.redirect('/login');
+});
+
 
 process.on('unhandledRejection', (err) => {
   console.log(`An error occurred: ${err.message}`);
