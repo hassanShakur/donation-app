448e57310a007d37dcf4346a9c1d6660c4081a93
hassanShakur <hass.shakurhassan@gmail.com>
2023-10-29 23:18:57 +0300

    fix view routing and add some styling

---
+++ server/routers/views.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+const viewsController = require('../controllers/views');
+
+router.get('/', viewsController.getIndex);
+router.get('/login', viewsController.getLogin);
+router.get('/register', viewsController.getRegister);
+router.get('/dashboard', viewsController.getDashboard);
+router.get('/profile', viewsController.getProfile);
+router.get('/logout', viewsController.getLogout);
+
+module.exports = router;
