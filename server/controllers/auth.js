230f0cb8b8ece744a6793003191da379a98bcd18
hassanShakur <hass.shakurhassan@gmail.com>
2023-10-29 19:09:19 +0300

    add login controller

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -26,3 +26,37 @@ exports.register = async (req, res, next) => {
     });
   }
 };
+
+exports.login = async (req, res, next) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res.status(400).json({
+      status: 'error',
+      message: 'Please provide username and password!',
+    });
+  }
+
+  try {
+    const user = User.findOne({ username, password });
+
+    if (!user) {
+      return res.status(401).json({
+        status: 'failed',
+        message: 'User not found!',
+      });
+    }
+
+    res.status(200).json({
+      status: 'success',
+      message: 'Login successful!',
+      user,
+    });
+  } catch (err) {
+    res.status(401).json({
+      status: 'error',
+      message: 'User creation failed!',
+      error: err.message,
+    });
+  }
+};
