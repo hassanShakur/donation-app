1aba3ebdc61ef72f951f53f82267a191939da9a6
hassanShakur <hass.shakurhassan@gmail.com>
2023-11-19 17:59:14 +0300

    add auth restrictions for donation model

--- models/Donation.js
+++ models/Donation.js
@@ -3,14 +3,14 @@ const mongoose = require('mongoose');
 const donationSchema = mongoose.Schema({
   name: {
     type: String,
-    required: true,
+    required: [true, 'Please provide a donation name'],
   },
   image: {
     type: String,
   },
   amount: {
     type: Number,
-    required: true,
+    required: [true, 'Please provide a donation amount'],
   },
   date: {
     type: Date,
