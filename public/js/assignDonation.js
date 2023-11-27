509e0d7a98f3ae58c45756241abfa27e79095020
hassanShakur <hass.shakurhassan@gmail.com>
2023-11-27 10:37:55 +0300

    add asign donation frontend logic

---
+++ public/js/assignDonation.js
@@ -0,0 +1,32 @@
+import { showAlert } from './alert';
+
+export const assignDonation = async (organizationId, donationId) => {
+  try {
+    const res = await fetch(
+      `/api/donations/${donationId}/assignDonation`,
+      {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ organizationId }),
+      }
+    );
+
+    const data = await res.json();
+    console.log(data);
+
+    if (data.status === 'success') {
+      showAlert('success', data.message);
+
+      window.setTimeout(() => {
+        location.reload(true);
+      }, 1500);
+    } else {
+      showAlert('error', data.message);
+    }
+  } catch (err) {
+    showAlert('error', 'Something went wrong!');
+    console.log(`An error occurred: ${err.message}`);
+  }
+};
